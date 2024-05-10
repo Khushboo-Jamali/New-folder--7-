dcccc3fc12693b41100e5d5dc4d36068689cb8bf
khushboo-jamali <jamalikhushboo@gmail.com>
2024-05-10 16:34:16 +0100

    i create new solar bar

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ document.getElementById("about").addEventListener("click", function () {
   document.getElementById("content").innerHTML =
     "<h2>About Us</h2><p>We are a team dedicated to creating fun and educational content for kids!</p>";
 });
+
+function createStars() {
+  const container = document.querySelector("body");
+  for (let i = 0; i < 1000; i++) {
+    // Increase the number of stars to 1000
+    const star = document.createElement("div");
+    star.className = "star";
+    star.style.width = ".1px";
+    star.style.height = ".1px";
+    star.style.top = Math.random() * 100 + "%";
+    star.style.left = Math.random() * 100 + "%";
+    container.appendChild(star);
+  }
+}
+createStars();
